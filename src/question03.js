c2bdb1dd556a142182944632cdf09d0624ebf84b
DOUGLAS MORAES DA SILVA <douglas_silva172@estudante.sesisenai.org.br>
2026-08-13 21:47:22 -0300

    commit prova

--- src/question03.js
+++ src/question03.js
@@ -3,6 +3,9 @@ import leia from 'readline-sync'
 var abastecimentos = leia.questionInt("INFORME QUANTOS ABASTECIMENTOS: ");
 
 var gasto = 0;
+var gasolina = 0
+var Etanol = 0;
+var Diesel = 0;
 
 for(var i = 1; i <= abastecimentos; i++) {
 
@@ -15,6 +18,21 @@ for(var i = 1; i <= abastecimentos; i++) {
 
     if(g === 1) {
         a = litros * 6.20
-        
-    }
+        gasto += a
+        gasolina += 1
+    } else if (g === 2) {
+        b = litros * 4.30
+        gasto += b
+        Etanol += 1
+    } else if (g === 3) {
+        c = litros * 4.30
+        gasto += c
+        Diesel += 1
+    } 
+
 }
+
+console.log("quantidade de abastecimentos Gasolina: " + gasolina)
+console.log("quantidade de abastecimentos Etanol: " + Etanol)
+console.log("quantidade de abastecimentos Diesel: " + Diesel)
+console.log("valor total arrecadado pelo posto: " + gasto)
